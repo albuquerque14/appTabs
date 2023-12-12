a14a93ae8537dc014d4cfc9de7aa3799935a8d10
albuquerque14 <127431207+albuquerque14@users.noreply.github.com>
2023-12-12 03:43:54 -0300

    appTabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,6 +12,16 @@ export class Tab1Page {
     this.likes++
   }
 
+  public produtos=[
+    { 
+        id: 1, 
+        produto: "Placa de Vídeo RTX 4060",
+        preco:"R$1939,99",
+        descricao:"Possui três ventiladores dee lâmina exclusivos de 80mm, tubos de calor de cobre compostos giratórios alternativos que tocam diretamente a GPU",
+        miniDescricao:"Possui três ventiladores de lâmina exclusivos de 80mm",
+        imagem:"https://static.netshoes.com.br/produtos/tenis-mizuno-brave-6/40/2FU-6366-040/2FU-6366-040_zoom1.jpg" 
+    },
+ ]
   constructor() {}
 
 }
